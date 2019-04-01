de3720e5d868c9aa76183bdec8e8a6e8db6b2156
MFaisca <MFaisca@HEAVEN.home>
2019-04-01 23:44:27 +0100

    .

--- java/com/materia/materiabot/Configs.java
+++ java/com/materia/materiabot/Configs.java
@@ -2,6 +2,6 @@ package com.materia.materiabot;
 
 public class Configs {
 	public static long OWNER_ID = 141599746987917312L;
-	public static String BOT_TOKEN = "Mzg3NTQ5NDkxNDAzODE2OTYx.XJ9joA.ugokEaW5MHTyHOfqa3ZhtkSsa0w";
+	public static String BOT_TOKEN = null;
 	public static String COMMAND_PREFIX = "!!";
 }
